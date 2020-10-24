86d62a8db0840e25b661e6916d700d52c3d652df
arenignacio <business.arenignacio@gmail.com>
2020-10-24 17:56:08 -0400

    add calculate barebones

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Calculate from './Calculate';
 /* 
 TODO:
 -add keypadd component
+-create calcuation functionality
  */
 
 function App(props) {
@@ -12,7 +13,7 @@ function App(props) {
 		<div className="container border border-dark">
 			<h5 className="pt-2">Calculator - Aren I.</h5>
 			<View />
-			<Calculate text={'Hello!'} performCalc={'test!!'} />
+			<Calculate />
 		</div>
 	);
 }
--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-function Calculate(props) {
+//take input
+
+//sort input and group numbers together while separating operators and operands
+
+//order of operations. look for parentheses and nest problems inside problems, solve deepest nested problem first while moving up in level as each problem is solved
+
+//send output to View
+
+/* function Calculate(props) {
 	console.log(props);
 	return (
-		<div className="p-2">{`${props.text} this is a ${props.performCalc} `}</div>
+		<div className="p-2">{`${props.text} this is a ${props.performCalc}. Nothing goes here, it's just`}</div>
 	);
+} */
+
+class Calculate extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			equation: [],
+			expression: '',
+		};
+	}
+
+	componentDidMount() {}
+
+	render() {
+		<div className="p-2">{`${props.text} this is a ${props.performCalc}. Nothing goes here, it's just`}</div>;
+	}
 }
 
 export default Calculate;
