4c69ce2c5b5aa520bfa647e6eb5fc8e2e139b505
arenignacio <business.arenignacio@gmail.com>
2020-11-02 00:24:09 -0500

    Re-add Keypad.js

---
+++ src/components/Keypad.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Text.scss';
+
+class Keypad extends React.Component {
+	constructor(props) {
+		super(props);
+
+		state: {
+		}
+	}
+
+	render() {
+		return <p className="fs-l">Keypad</p>;
+	}
+}
+
+export default Keypad;
