d65f47ddc21daed17adfa57265848c1b046ac074
arenignacio <business.arenignacio@gmail.com>
2020-12-21 00:40:25 -0500

    Implement keyboard functionality
    Added key press effect to corresponding button.
    Added event listener that invokes addToView callback.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,12 @@ import isOperator from './helper_functions/isOperator';
 
 /* memory buttons disabled */
 /* 
-TODO: keydown events for later update. Key identifiers already written in switch case.
+TODO: 
+
+// - keydown events for later update. Key identifiers already written in switch case.
+// -addToView function moved to global. needs refactoring for renderbtn function to use addToView. 
+
+-keydown event does not stack. button click does.
 */
 
 const Keypad = ({
@@ -16,167 +21,231 @@ const Keypad = ({
 	init,
 	hideProblem,
 }) => {
+	//NOTE: addToView now has parameter. older version did not use an argument.
+
 	const ref = useRef();
 
-	useEffect(() => {
-		window.addEventListener('keydown', (event) => {
-			const keypadChars = ['Backspace', 'Delete', 'c', 'C', '.'];
+	const addToView = (el) => {
+		console.log(`el is ${el}`);
+		console.log(`problem is ${problem}`);
 
-			if (
-				event.key &&
-				(keypadChars.includes(event.key) ||
-					isOperator(event.key) ||
-					!isNaN(event.key))
-			) {
-				console.log('key pressed ' + event.key);
+		//value props
+		const appSolution = solution;
+		const appisProblemHidden = isProblemHidden;
+		let newProblemArr = Array.from(problem);
+		let lastChar = newProblemArr[newProblemArr.length - 1];
+
+		//function props
+		const appInit = init;
+		const apphideProblem = hideProblem;
+
+		switch (el) {
+			case 'CE':
+				return appInit(newProblemArr.join(''));
+
+			case 'c':
+			case 'C':
+				apphideProblem();
+				return appInit();
+
+			case '=':
+			case 'Enter':
+				return appInit(0, appSolution);
+
+			case 'mc':
+			case 'mr':
+			case 'm+':
+			case 'm-':
+			case 'ms':
+				return;
+			case '+/-':
+				if (newProblemArr[0] === '-') {
+					newProblemArr.shift();
+					newProblemArr.unshift('+');
+				} else if (newProblemArr[0] === '+') {
+					newProblemArr.shift();
+					newProblemArr.unshift('-');
+				}
+
+				if (!isNaN(newProblemArr[0])) {
+					newProblemArr.unshift('+');
+				}
+
+				break;
+
+			case ')':
+				if (lastChar !== '(') {
+					newProblemArr.push(')');
+
+					if (
+						newProblemArr.length === 1 ||
+						isGreaterThan(')', '(', newProblemArr)
+					) {
+						newProblemArr.pop();
+					}
+				}
+				break;
+
+			case 'Backspace':
+			case 'Delete':
+			case 'DEL':
+				newProblemArr.pop();
+
+				if (
+					newProblemArr.length === 1 &&
+					(isOperator(newProblemArr[0]) ||
+						['(', ')'].includes(newProblemArr[0]))
+				) {
+					newProblemArr.pop();
+				}
+
+				if (newProblemArr.length === 0) {
+					apphideProblem();
+					return appInit();
+				}
+
+				break;
+
+			case '.':
+				if (isOperator(lastChar) || lastChar === '.') {
+					newProblemArr.pop();
+				}
+
+				//prevent two decimals in one number
+				const stack = newProblemArr.filter(
+					(el) => isOperator(el) || ['(', ')', '.'].includes(el)
+				);
+				stack.push(el);
+				const stackStr = stack.join('');
+
+				if (!stackStr.includes('..')) {
+					appisProblemHidden
+						? (newProblemArr = [appSolution, el])
+						: newProblemArr.push(el);
+				}
+
+				break;
+
+			case 'X':
+			case 'x':
+				if (isOperator(lastChar)) {
+					newProblemArr.pop();
+				}
+
+				newProblemArr.push('*');
+				if (appisProblemHidden && appSolution !== '0') {
+					newProblemArr = [appSolution, '*'];
+				}
+
+				break;
+
+			default:
+				if (isOperator(el)) {
+					if (isOperator(lastChar)) {
+						newProblemArr.pop();
+					}
+
+					if (appisProblemHidden) {
+						newProblemArr = [appSolution];
+					}
+				}
+				newProblemArr.push(el);
+
+				break;
+		}
+
+		console.log('newProblemArr: ' + newProblemArr.join(''));
+		showProblem();
+		hClick(newProblemArr.join(''));
+	};
+
+	const pressEffect = (key) => {
+		switch (key) {
+			case 'Backspace':
+			case 'Delete':
+				key = 'DEL';
+				break;
+			case 'Enter':
+				key = '=';
+				break;
+			case '*':
+				key = 'x';
+				break;
+			case 'c':
+				key = 'C';
+				break;
+			default:
+				break;
+		}
+
+		const selectedNode = document.getElementById(key);
+
+		console.log(selectedNode);
+		selectedNode.focus();
+
+		setTimeout(() => {
+			selectedNode.blur();
+		}, 150);
+	};
+
+	useEffect(() => {
+		const handleKeydown = (event) => {
+			const keypadChars = [
+				'Backspace',
+				'Delete',
+				'c',
+				'C',
+				'.',
+				'=',
+				'Enter',
+				'(',
+				')',
+			];
+
+			let { key } = event;
+
+			if (keypadChars.includes(key) || isOperator(key) || !isNaN(key)) {
+				console.log('key pressed ' + key);
+
+				pressEffect(key);
+				addToView(key);
 			}
-		});
-	}, []);
+		};
+
+		window.addEventListener('keydown', handleKeydown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	});
+
+	useEffect(() => {
+		console.log('re-render');
+	});
 
 	const defaultStyle =
 		'font-weight-bold btn-keypad border-grey rounded p-2 m-2 text-center btn unselectable';
 
 	const renderBtn = (arr, style, size = '2', callBack) => {
 		return arr.map((el) => {
-			const addToView = () => {
-				//value props
-				let appProblem = problem;
-				const appSolution = solution;
-				const appisProblemHidden = isProblemHidden;
-				let newProblemArr = Array.from(appProblem);
-				let lastChar = newProblemArr[newProblemArr.length - 1];
+			const callAddToView = () => {
+				//unfocus on button after 300ms
+				setTimeout(() => {
+					document.getElementById(el).blur();
+				}, 300);
 
-				//function props
-				const appInit = init;
-				const apphideProblem = hideProblem;
-
-				switch (el) {
-					case 'CE':
-						return appInit(newProblemArr.join(''));
-
-					case 'c':
-					case 'C':
-						apphideProblem();
-						return appInit();
-
-					case '=':
-						return appInit(0, appSolution);
-
-					case 'mc':
-					case 'mr':
-					case 'm+':
-					case 'm-':
-					case 'ms':
-						return;
-					case '+/-':
-						if (newProblemArr[0] === '-') {
-							newProblemArr.shift();
-							newProblemArr.unshift('+');
-						} else if (newProblemArr[0] === '+') {
-							newProblemArr.shift();
-							newProblemArr.unshift('-');
-						}
-
-						if (!isNaN(newProblemArr[0])) {
-							newProblemArr.unshift('+');
-						}
-
-						break;
-
-					case ')':
-						if (lastChar !== '(') {
-							newProblemArr.push(')');
-
-							if (
-								newProblemArr.length === 1 ||
-								isGreaterThan(')', '(', newProblemArr)
-							) {
-								newProblemArr.pop();
-							}
-						}
-						break;
-
-					case 'Backspace':
-					case 'Delete':
-					case 'DEL':
-						newProblemArr.pop();
-
-						if (
-							(newProblemArr.length === 1 &&
-								isOperator(newProblemArr[0])) ||
-							['(', ')'].includes(newProblemArr[0])
-						) {
-							newProblemArr.pop();
-						}
-
-						if (newProblemArr.length === 0) {
-							apphideProblem();
-							return appInit();
-						}
-
-						break;
-
-					case '.':
-						if (isOperator(lastChar) || lastChar === '.') {
-							newProblemArr.pop();
-						}
-
-						//prevent two decimals in one number
-						const stack = newProblemArr.filter(
-							(el) => isOperator(el) || ['(', ')', '.'].includes(el)
-						);
-						stack.push(el);
-						const stackStr = stack.join('');
-
-						if (!stackStr.includes('..')) {
-							appisProblemHidden
-								? (newProblemArr = [appSolution, el])
-								: newProblemArr.push(el);
-						}
-
-						break;
-
-					case 'X':
-					case 'x':
-						if (isOperator(lastChar)) {
-							newProblemArr.pop();
-						}
-
-						newProblemArr.push('*');
-						if (appisProblemHidden && appSolution !== '0') {
-							newProblemArr = [appSolution, '*'];
-						}
-
-						break;
-
-					default:
-						if (isOperator(el)) {
-							if (isOperator(lastChar)) {
-								newProblemArr.pop();
-							}
-
-							if (appisProblemHidden) {
-								newProblemArr = [appSolution];
-							}
-						}
-						newProblemArr.push(el);
-
-						break;
-				}
-
-				console.log('newProblemArr: ' + newProblemArr.join(''));
-				showProblem();
-				hClick(newProblemArr.join(''));
+				//calculate then add to view
+				addToView(el);
 			};
 
 			return (
 				<input
 					key={el}
+					id={el}
 					type="button"
 					className={`${defaultStyle} col-${size} ${style}`}
 					value={el}
-					onClick={callBack || addToView}
-					onKeyDown={addToView}
+					onClick={callBack || callAddToView}
 				/>
 			);
 		});
