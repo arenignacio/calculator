63c2e69e37d10549cdfe1cf4eee070de95691a20
arenignacio <55777461+arenignacio@users.noreply.github.com>
2024-11-20 07:35:08 -0500

    Rework (#2)
    * add node version in package.json
    
    * readability

--- src/components/View.js
+++ src/components/View.js
@@ -3,16 +3,10 @@ import React from 'react';
 const View = ({ solution, problem, sizeModifier }) => {
 	return (
 		<div className="parent d-flex flex-column justify-content-end pr-3 py-1 ml-3 mr-3 viewport rounded">
-			<span
-				className={`d-inline-block text-wrap d-flex flex-row justify-content-end fs-${sizeModifier}`}
-				id="solution"
-			>
+			<span className={`d-inline-block text-wrap d-flex flex-row justify-content-end fs-${sizeModifier}`} id="solution">
 				<strong>{solution || 0}</strong>
 			</span>
-			<div
-				className="equation d-flex flex-row justify-content-end text-black-50 font-weight-bold ls-1"
-				id="problem"
-			>
+			<div className="equation d-flex flex-row justify-content-end text-black-50 font-weight-bold ls-1" id="problem">
 				{problem || ''}
 			</div>
 		</div>
